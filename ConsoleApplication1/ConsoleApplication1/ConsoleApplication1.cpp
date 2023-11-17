c63b5deb5724cac76697dae31b9bc50119e4ca36
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-17 12:36:27 +0100

    141234213

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,298 @@
+﻿﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void task1()
+{
+	double numberFromUser;
+
+	do
+	{
+		cout << "Enter the number: " << endl;
+		cin >> numberFromUser;
+	} while (numberFromUser != 0);
+	cout << "The number equals 0. " << endl;
+}
+void task2()
+{
+	int firstNumberFromUser;
+	int secondNumberFromUser;
+
+	cout << "Enter first number: " << endl;
+	cin >> firstNumberFromUser;
+	cout << "Enter second number: " << endl;
+	cin >> secondNumberFromUser;
+
+	while (firstNumberFromUser > 0 && secondNumberFromUser > 0)
+	{
+
+		if (firstNumberFromUser == secondNumberFromUser)
+		{
+			cout << "Greatest common divisor of those two nubers is: " << firstNumberFromUser << endl;
+			return;
+		}
+
+		if (firstNumberFromUser > secondNumberFromUser)
+		{
+			firstNumberFromUser = firstNumberFromUser - secondNumberFromUser;
+		}
+		else
+		{
+			secondNumberFromUser = secondNumberFromUser - firstNumberFromUser;
+		}
+	}
+}
+
+void task3()
+{
+	double sumOfNumbers = 0.0;
+	do
+	{
+		double numberFromUser;
+		cout << "Enter a number: " << endl;
+		cin >> numberFromUser;
+
+		sumOfNumbers = sumOfNumbers + numberFromUser;
+
+		if (numberFromUser == 0)
+		{
+			cout << "Sum of numbers is: " << sumOfNumbers;
+			return;
+		}
+	} while (true);
+}
+//* Program wyświetlający na ekranie kolejne liczby naturalne od 1 do 10
+void task4()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << i << endl;
+	}
+}
+
+//* Program obliczający sumę liczb od 1 do 100
+void task5()
+{
+	int sum = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		sum = sum + i;
+	}
+	cout << sum << endl;
+}
+
+//*Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
+void task6()
+{
+	for (int i = 1; i < 10; i++)
+	{
+		double powerOfNumber = pow(i, 2);
+		cout << powerOfNumber << endl;
+	}
+}
+//	* Program obliczający n!.
+void task7()
+{
+	int numberFromUser;
+	cout << "Enter number: " << endl;
+	cin >> numberFromUser;
+	int sum = 1;
+	for (int i = 1; i <= numberFromUser; i++)
+	{
+		sum = sum * i;
+	}
+	cout << "Factorial equals: " << sum << endl;
+}
+//Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4!itd.)
+void task8()
+{
+	int factorial = 1;
+	for (int i = 1; i <= 10; i++)
+	{
+		factorial = factorial * i;
+		cout << "Factorials 1-10 equals: " << factorial << endl;
+	}
+}
+//* Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1 = 1, 1x2 = 2, 1x3 = 3 itd.).
+void task9()
+{
+
+	for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			cout << i << "*" << j << "=" << i * j << "  ";
+		}
+		cout << endl;
+	}
+}
+//*Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+void task10()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			cout << i << endl;
+		}
+	}
+}
+//*Program obliczający sumę kwadratów liczb od 1 do 10
+void task11()
+{
+	int sum = 0;
+	for (int i = 0; i <= 10; i++)
+	{
+		sum = sum + i * i;
+	}
+	cout << sum;
+}
+//* Napisz program, który policzy sumę cyfr podanej przez użytkownika liczby.
+void task12()
+{
+	double numberFromUser = 0;
+	double sum = 0;
+
+	do
+	{
+		cout << "Enter number: " << endl;
+		cin >> numberFromUser;
+		sum = sum + numberFromUser;
+		cout << "Sum: " << sum << endl;
+	} while (true);
+}
+// Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.) dopóki suma tych kwadratów nie przekroczy 1000.
+void task13()
+{
+	int sum = 0;
+	for (int i = 1; sum < 1000; i++)
+	{
+		if (i >= 10)
+		{
+			i = 1;
+		}
+		sum = sum + i * i;
+		cout << sum << endl;
+	}
+}
+//* Program, kóry wyświetli poniższe cztery wzory :
+// *
+// **
+// ***
+// ****
+// *****
+void task14()
+{
+	int numberFromUser = 0;
+	cout << "Enter number: " << endl;
+	cin >> numberFromUser;
+	for (int i = 0; i <= numberFromUser; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+/* Napisz program, który poprosi użytkownika o wprowadzenie dowolnej liczby całkowitej.Następnie program powinien obliczyć i wyświetlić liczbę cyfr.
+*/
+void task15()
+{
+	int numberFromUser = 0;
+	cout << "Enter number from user: " << endl;
+	cin >> numberFromUser;
+	cout << to_string(numberFromUser).length();
+}
+//* Miasto T.ma obecnie 100 tys.mieszkańców, ale jego populacja rośnie co roku o 3 % rocznie.Miasto B.ma 300 tys.mieszkańców 
+// i ta liczba rośnie w tempie 2 % na rok.Wykonaj symulację prezentującą liczbę mieszkańców w obu miastach i zatrzymującą się, gdy liczba mieszkańców miasta T.przekroczy liczbę z miasta B.
+void task16()
+{
+	int firstCity = 100000;
+	int secondCity = 300000;
+
+	for (int i = 0; firstCity < secondCity; i++)
+	{
+		firstCity = firstCity * 1.03;
+		secondCity = secondCity * 1.02;
+		cout << firstCity << "  " << secondCity << endl;
+	}
+	cout << firstCity << "  " << secondCity << endl;
+}
+
+//Oblicz sumę szeregu 1 / 1 ^ 2 + 1 / 2 ^ 2 + 1 / 3 ^ 2 + ... + 1 / n ^ 2. 
+// Jeżeli się nie pomylisz, to dla odpowiednio dużej wartości n po przemnożeniu jej przez 6 i
+//  spierwiastkowaniu powinieneś otrzymać wartość liczby π(suma szeregu jest równa π2 / 6).Zwróć uwagę, że pierwsza wartość indeksu to 1, a nie 0. Dokładność obliczeń można sprawdzić porównując z wartością odczytaną z własności Math.PI.
+void task17()
+{
+	double sum = 0;
+	double numberFromUser = 0;
+	cout << "Enter number: " << endl;
+	cin >> numberFromUser;
+	for (int i = 1; i < numberFromUser; i++)
+	{
+		sum = sum + (1.0 / pow(i, 2));
+	}
+	cout << "Sum: " << sqrt(sum * 6) << endl;
+}
+
+void task18()
+{
+	int sizeOfPyramid = 0;
+	cout << "Enter number: " << endl;
+	cin >> sizeOfPyramid;
+	for (int row = 0; row < sizeOfPyramid; row++)
+	{
+
+		for (int spaces = 0; spaces < sizeOfPyramid - row - 1; spaces++)
+		{
+			cout << " ";
+
+		}
+		for (int stars = 0; stars <= row * 2; stars++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+
+/*DO - WHILE
+
+WHILE
+
+
+* Program sprawdzający czy podana liczba jest liczbą doskonałą(czyli taką, której suma dzielników(z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
+
+FOR
+* Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu(ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
+
+
+
+*/
+
+
+int main()
+{
+	//task1();
+	//task2();
+	//task3();
+	//task4();
+	//task5();
+	//task6();
+	//task7();
+	//task8();
+	//task9();
+	//task10();
+	//task11();
+	//task12();
+	//task13();
+	//task14();
+	//task15();
+	//task16();
+	//task17();
+	task18();
+}
---
+++ ConsoleApplication1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,353 @@
+﻿﻿#include <iostream>
+using namespace std;
+#include <ctime>
+void task()
+{
+	double firstNumber;
+	double secondNumber;
+	double quotient;
+	cout << "Enter the first number: " << endl;
+	cin >> firstNumber;
+	cout << "Enter the second number: " << endl;
+	cin >> secondNumber;
+	quotient = firstNumber * secondNumber;
+	cout << "Quotient score of two numbers is: " << quotient << endl;
+}
+
+//1.Program sprawdzający czy podana liczba jest parzysta czy nieparzysta
+void task1()
+{
+	int number;
+	cout << "Enter the number: " << endl;
+	cin >> number;
+	if (number % 2 == 0)
+	{
+		cout << "The number is even." << endl;
+	}
+	else
+	{
+		cout << "The number is odd." << endl;
+	}
+}
+
+//2. Program sprawdzający czy podana liczba jest dodatnia, ujemna czy równa zero
+void task2()
+{
+	int number;
+	cout << "Enter the number: " << endl;
+	cin >> number;
+	if (number > 0)
+	{
+		cout << "The number is higher then 0." << endl;
+	}
+	else if (number == 0)
+	{
+		cout << "The number equal 0." << endl;
+	}
+	else
+	{
+		cout << "The number is lower then 0." << endl;
+	}
+}
+
+//3. Program sprawdzający czy podany rok jest rokiem przestępnym
+void task3()
+{
+	int year;
+	cout << "Enter the year: " << endl;
+	cin >> year;
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+	{
+		cout << "This a leap year." << endl;
+	}
+	else
+	{
+		cout << "This isn't a leap year." << endl;
+	}
+}
+
+//4. Program wyświetlający odpowiedni komunikat w zależności od podanej oceny(np. "bardzo dobry" dla oceny 5, "dobry" dla oceny 4 itd.)
+void task4()
+{
+	int grade;
+	cout << "Enter the grade: " << endl;
+	cin >> grade;
+	if (grade == 6)
+	{
+		cout << "That is excellent grade." << endl;
+	}
+	else if (grade == 5)
+	{
+		cout << "That is very good grade." << endl;
+	}
+	else if (grade == 4)
+	{
+		cout << "That is good grade." << endl;
+	}
+	else if (grade == 3)
+	{
+		cout << "That is average grade." << endl;
+	}
+	else if (grade == 2)
+	{
+		cout << "That is very acceptable grade." << endl;
+	}
+	else if (grade == 1)
+	{
+		cout << "That is failing grade." << endl;
+	}
+	else {
+		cout << "This isn't grade in the range 1-6." << endl;
+	}
+}
+
+//5. Program sprawdzający czy podane hasło jest poprawne(np.jeśli hasło jest "abc123",
+//  program powinien wyświetlić "hasło poprawne", jeśli jest inne, powinien wyświetlić "hasło niepoprawne").
+void task5()
+{
+	string password;
+	cout << "Enter the password: " << endl;
+	cin >> password;
+	if (password == "abc123")
+	{
+		cout << "Password is correct." << endl;
+	}
+	else
+	{
+		cout << "Passowrd isn't correct." << endl;
+	}
+}
+
+//6. Program sprawdzający czy podana data jest poprawna(np.sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
+void task6()
+{
+	int day;
+	int month;
+	int year;
+	cout << "Enter the day: " << endl;
+	cin >> day;
+	cout << "Enter the month: " << endl;
+	cin >> month;
+	cout << "Enter the year: " << endl;
+	cin >> year;
+	bool  isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	if (
+		day < 1 || day >31 || month < 1 || month > 12
+		|| (day == 31 && (month == 4 || month == 6 || month == 9 || month == 11))
+		|| (month == 2 && day > 29)
+		|| (month == 2 && day == 29 && !isLeapYear)
+		)
+	{
+		cout << "The date isn't correct. " << endl;
+		return;
+	}
+	cout << "Date is correct." << endl;
+}
+
+//Program wyświetlający odpowiedni komunikat w zależności od podanej temperatury
+//(np. "ciepło" dla temperatury powyżej 20 stopni Celsjusza, "chłodno" dla temperatury poniżej 10 stopni Celsjusza itd.)
+void task7()
+{
+	double temperature;
+	cout << "Enter the temperature: " << endl;
+	cin >> temperature;
+	if (temperature > 20)
+	{
+		cout << "It's hot." << endl;
+	}
+	else if (temperature < 10)
+	{
+		cout << "It's cold." << endl;
+	}
+	else
+	{
+		cout << "It's an average temperature." << endl;
+	}
+}
+//8. Program sprawdzjący czy podana liczba jest z przediału <1;15)
+void task8()
+{
+	double number;
+	cout << "Enter the number: " << endl;
+	cin >> number;
+	if (number >= 1 && number <= 15)
+	{
+		cout << "The number is in the range from 1 to 15." << endl;
+	}
+	else
+	{
+		cout << "The number isn't in the range." << endl;
+	}
+}
+//9. Program sprawdzający czy osoba jest pełnoletnia.
+void task9()
+{
+	int personAge;
+	cout << "Enter the age: " << endl;
+	cin >> personAge;
+	if (personAge >= 18)
+	{
+		cout << "The person is an adult." << endl;
+	}
+	else if (personAge < 18 && personAge>0)
+	{
+		cout << "The person is a minor." << endl;
+	}
+	else
+	{
+		cout << "The age isn't correct. " << endl;
+	}
+}
+//10. Program, który sprawdzi czy z podanych długości boków można zrobić trójkąt.
+void task10()
+{
+	double firstSide;
+	double secondSide;
+	double thirdSide;
+	cout << "Enter first side of triangle: " << endl;
+	cin >> firstSide;
+	cout << "Enter second side of triangle: " << endl;
+	cin >> secondSide;
+	cout << "Enter third side of triangle: " << endl;
+	cin >> thirdSide;
+	if ((firstSide + secondSide > thirdSide) && (secondSide + thirdSide > firstSide) && (thirdSide + firstSide > secondSide))
+	{
+		cout << "From those sides you can make a triangle." << endl;
+	}
+	else
+	{
+		cout << "You can't do triangle with those sides." << endl;
+	}
+}
+//11. Napisz program, który będzie działać jako prosty kalkulator matematyczny, umożliwiając użytkownikowi wybór 
+// operacji(dodawanie, odejmowanie, mnożenie, dzielenie) i wprowadzanie liczb do obliczeń.
+void task11()
+{
+	char operation;
+	double firstNumber;
+	double secondNumber;
+
+	cout << "Choose an operation: + (addition), - (subtraction), * (multiplication), / (division)" << endl;
+	cin >> operation;
+	cout << "Enter the first number: " << endl;
+	cin >> firstNumber;
+	cout << "Enter the second number: " << endl;
+	cin >> secondNumber;
+
+	switch (operation)
+	{
+	case '+':
+		cout << "Addition result of those two numbers is: " << firstNumber + secondNumber << endl;
+		break;
+	case '-':
+		cout << "Subtraction result of those two numbers is: " << firstNumber - secondNumber << endl;
+		break;
+	case '*':
+		cout << "Multiplication result of those two numbers is: " << firstNumber * secondNumber << endl;
+		break;
+	case '/':
+		cout << "Subtraction result of those two numbers is: " << firstNumber - secondNumber << endl;
+		break;
+	default:
+		cout << "Invaild operation. Please use '+', '-', '*', or '/'." << endl;
+	}
+}
+//12. Napisz program, który wczyta numer miesiąca i wyświetli jego słowny odpowiednik.
+void task12()
+{
+	int month;
+	cout << "Enter the number of the month: " << endl;
+	cin >> month;
+
+	switch (month)
+	{
+	case 1:
+		cout << "January" << endl;
+		break;
+	case 2:
+		cout << "February" << endl;
+		break;
+	case 3:
+		cout << "March" << endl;
+		break;
+	case 4:
+		cout << "April" << endl;
+		break;
+	case 5:
+		cout << "May" << endl;
+		break;
+	case 6:
+		cout << "June" << endl;
+		break;
+	case 7:
+		cout << "July" << endl;
+		break;
+	case 8:
+		cout << "August" << endl;
+		break;
+	case 9:
+		cout << "September" << endl;
+		break;
+	case 10:
+		cout << "October" << endl;
+		break;
+	case 11:
+		cout << "November" << endl;
+		break;
+	case 12:
+		cout << "December" << endl;
+		break;
+	default:
+		cout << "There is no such month." << endl;
+	}
+}
+//13. Napisz program, który na podstawie współczynników równania kwadratowego(a, b, c)
+//  sprawdzi, czy to równanie ma rozwiązania rzeczywiste, i jeśli tak, to je obliczy.
+void task13()
+{
+	double coefficientA;
+	double coefficientB;
+	double coefficientC;
+	cout << "Enter the first coefficent A: " << endl;
+	cin >> coefficientA;
+	cout << "Enter the second coefficent B: " << endl;
+	cin >> coefficientB;
+	cout << "Enter the third coefficent C: " << endl;
+	cin >> coefficientC;
+	double delta = coefficientB * coefficientB - 4 * coefficientA * coefficientC;
+	if (delta > 0)
+	{
+		double x1 = (-coefficientB + sqrt(delta)) / (2 * coefficientA);
+		double x2 = (-coefficientB - sqrt(delta)) / (2 * coefficientA);
+		cout << "The equation has two real roots." << endl;
+		cout << "x1: " << x1 << endl;
+		cout << "x2: " << x2 << endl;
+	}
+	else if (delta == 0)
+	{
+		double x1 = -coefficientB / (2 * coefficientA);
+		cout << "The equation has only one real root." << endl;
+		cout << "x1: " << x1 << endl;
+	}
+	else
+	{
+		cout << "The equation doesn't have any real roots." << endl;
+	}
+}
+int main()
+{
+	//task();
+	//task1();
+	//task2();
+	//task3();
+	//task4();
+	//task5();
+	//task6();
+	//task7();
+	//task8();
+	//task9();
+	//task10();
+	//task11();
+	//task12();
+	//task13();
+}
